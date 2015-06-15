f0f791d87c295d3de82698a73eb220ec94943105
Guillaume Perrin <gperrin01@gmail.com>
2015-06-15 19:59:38 +0100

    ajax now adding a new to_Eat

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,21 +4,30 @@ $(function() {
   $.get('/foods').
     done(function(data) {
       var object = JSON.parse(data);
+      // why did we send JSON on the get / but not Post?
       $(object).each(function(index, toeat) {
-        console.log(index, toeat);
-        addToEat(toeat);
+        addToEat(toeat, toeat.id + 1);
       })
     })
 
-
+  $('#new_toeat').on('submit', function () {
+    event.preventDefault();
+    var $this = $(this);
+    var formData = $this.serialize();
+    $.post('/foods', formData).
+      done(function(data) {
+      // why did we send JSON on the get / but not Post?
+      addToEat(data.toeat, data.id);
+      })
+  })
 
 // end of event listeners
 }) 
 
-function addToEat (toeat) {
+function addToEat (toeat, index) {
   var $foods = $('#foods_container');
-  var $new = $("<div>" + toeat.name + " our verdict: " + toeat.yumminess + "<div.");
+  var $new = $("<div data-id='" +index + "'>" + toeat.name + " -> our verdict: " + toeat.yumminess + "<div>");
   $new.addClass('toeat');
-  $new.data('id', toeat.id +1);
+  // $new.data('id', id);
   $foods.append($new)
 }
